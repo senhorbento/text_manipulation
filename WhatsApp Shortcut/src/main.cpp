643de2b232f5288c478a6b15ea0ccbc926b64de4
Henrique Bento <68090144+senhorbento@users.noreply.github.com>
2022-02-10 17:22:52 -0300

    Update and rename WhatsApp Shortcut/main.cpp to WhatsApp Shortcut/src/main.cpp

--- WhatsApp Shortcut/src/main.cpp
+++ WhatsApp Shortcut/src/main.cpp
@@ -2,19 +2,19 @@
 
 using namespace std;
 
-#define _MAX_QTD_ 90
+#define _MAX_QTD_ 100
 
 int main() {
 	string prefix = "https://api.whatsapp.com/send/?phone=55";
-	string resultado[_MAX_QTD_],leitura;
+	string resultado[_MAX_QTD_], leitura;
 
-	for(int i=0;i<_MAX_QTD_;i++){
+	for(int i=0; i<_MAX_QTD_; i++){
 		cin >> leitura;
-		resultado[i] = prefix+leitura;
+		resultado[i] = prefix + leitura;
 	}
-	for(int i=0;i<_MAX_QTD_;i++){
+	
+	for(int i=0; i<_MAX_QTD_; i++)
 		cout << resultado[i] << endl;
-	}
 
 	return 0;
 }
