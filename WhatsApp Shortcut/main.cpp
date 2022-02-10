8e1d877cc78044c56700fc9b8d866aad2f6eaa18
Henrique Bento <68090144+senhorbento@users.noreply.github.com>
2022-02-10 17:11:28 -0300

    Create main.cpp

---
+++ WhatsApp Shortcut/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+#define _MAX_QTD_ 90
+
+int main() {
+  string prefix = "https://api.whatsapp.com/send/?phone=55";
+	string resultado[_MAX_QTD_],leitura;
+	
+	for(int i=0;i<_MAX_QTD_;i++){
+	    cin >> leitura;
+	    resultado[i] = prefix+leitura;
+	}
+	for(int i=0;i<_MAX_QTD_;i++){
+	    cout << resultado[i] << endl;
+	}
+	
+	return 0;
+}
